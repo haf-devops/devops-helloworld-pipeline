c80ba144b5495dc6f6619f5310400273b56b1633
haf-devops <haf.devops@gmail.com>
2026-07-09 00:19:40 -0400

    removed providers.tf file and added back to main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.80"
+    }
+  }
+  required_version = "~> 1.15.0"
+
+  backend "azurerm" {
+    resource_group_name = "devops-helloworld-pipeline"
+    storage_account_name = "storageaccount2697"
+    container_name       = "tfstatefile"
+    key                   = "devops-project.tfstate"
+    }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
 data "azurerm_resource_group" "rg" {
   name     = "devops-helloworld-pipeline"
 }
