7074777a85a2fee3a507579222ba8c8f926e1216
haf-devops <haf.devops@gmail.com>
2026-07-08 21:24:12 -0400

    added backend block to providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,8 +6,11 @@ terraform {
     }
   }
   required_version = "~> 1.15.0"
-}
 
-provider "azurerm" {
-  features {}
+  backend "azurerm" {
+    resource_group_name = "devops-helloworld-pipeline"
+    storage_account_name = "storageaccount2697"
+    container_name       = "tfstatefile"
+    key                   = "devops-project.tfstate"
+    }
 }
